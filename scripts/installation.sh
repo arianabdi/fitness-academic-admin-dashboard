e9880f6c2a0a1ee9fd76273c0cfc9b34cd294470
Arian Abdi <abdiarian2@gmail.com>
2023-09-17 13:16:51 +0330

    initial commit

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -20,12 +20,33 @@ if ! command -v nest &>/dev/null; then
   sudo npm install -g @nestjs/cli
 fi
 
+if ! command -v 7z &> /dev/null; then
+    echo "Installing 7z..."
+
+    # Check for package manager and use the appropriate installation command
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install p7zip
+    elif command -v yum &> /dev/null; then
+        sudo yum install p7zip
+    elif command -v brew &> /dev/null; then
+        brew install p7zip
+    else
+        echo "Could not install 7z. Please install it manually."
+        exit 1
+    fi
+fi
+
+
 
 # Install NestJS CLI (if not already installed)
 if ! command -v pm2 &>/dev/null; then
   echo "Installing pm2..."
   sudo npm install -g pm2
 fi
+
+
+
 
 
 # Clone your GitHub repository (replace <your-github-repository-url> with your actual repository URL)
@@ -35,18 +56,40 @@ git clone "https://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@github.com/arianabdi
 # Navigate to the project directory
 cd fitness-academic-admin-dashboard
 
+# Check if the RAR file exists
+lib="lib.rar"
+assets="assets.rar"
+
+if [ ! -f "$lib" ]; then
+    echo "$lib not found."
+    exit 1
+fi
+
+if [ ! -f "$assets" ]; then
+    echo "$assets not found."
+    exit 1
+fi
+
+# Extract the RAR file
+7z x "$lib"
+7z x "$assets"
+
+# Check the exit status of 7z
+if [ $? -eq 0 ]; then
+    echo "Extraction successful."
+else
+    echo "Extraction failed."
+fi
+
 # Install project dependencies
 echo "Installing node modules..."
 npm install
 
-# Build the NestJS application
-echo "Building the NestJS application..."
-npm run build
-
 # Start the application using pm2 (assuming pm2 is installed)
 echo "Starting the application"
+
 #npm start dist/main.js
-pm2 start dist/main.js --name fitness-academic-admin-dashboard
+pm2 start npm --name "fitness-academic-admin-dashboard" -- start
 
 #start application after server reboot
 pm2 startup
